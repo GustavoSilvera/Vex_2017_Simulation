e33120363b29dda6ad459615076e8fccb809552a
GustavoSilvera <gustavo.r.silvera@gmail.com>
2017-07-11 07:27:36 -0700

    added new field simulation for auton training

---
+++ include/field.h
@@ -0,0 +1,29 @@
+#include "cinder/gl/gl.h"
+#include "cinder/gl/Texture.h"
+#include "vec3.h"
+#include <vector>
+//declares the class for the robot and all the data that goes with it.
+//LOOKIE HERE: http://vexcompetition.es/wp-content/uploads/2017/04/IntheZone-Field-specifications.pdf
+class field {
+public:
+	field();
+
+	void FieldUpdate();
+	void initializeField();
+	//static  int NUMBER_OF_CONES = 50;
+	struct cone {
+		vec3 pos;//position
+		int heading;//direction of the cone in 360° when tipped
+		bool tipped;//if fallen 
+	};
+	std::vector<cone> c;//vector of cones
+
+	ci::gl::Texture coneTexture;
+	ci::gl::Texture fieldBare;
+	ci::gl::Texture fieldFull;
+
+	float fieldSize = 141.05;// 140.5 + 2 * (1.27);wall thickness accounted for
+	int dFromEdge = 100;//draw distance from the start of the field verticie to the edge of the window
+	bool initialized;//if the field bare texture is visible or not. 
+};
+
---
+++ src/field.cpp
@@ -0,0 +1,78 @@
+#include "vec3.h"
+#include "field.h"
+#include "randomstuff.h"
+
+//declares and defines the field class and functions
+
+field::cone initialConfiguration[54] = {//array for each configuration of the cone (in field.h)
+	{ { 2.9, 2.9 }, 0, false},//position X,Y; initial heading; if tipped or not
+	{ { 2.9, 13.0 }, 0, false},
+	{ { 2.9, 23.2 }, 0, false},
+	{ { 2.9, 34.9 }, 0, false},
+	{ { 2.9, 46.7 }, 0, false},
+	{ { 2.9, 58.4 }, 0, false},
+	{ { 2.9, 70.2 }, 0, false},
+	{ { 13.0, 2.9 }, 0, false},
+	{ { 13.0, 13.0 }, 0, false},
+	{ { 13.0, 23.2 }, 0, false},
+	{ { 23.2, 2.9 }, 0, false},
+	{ { 23.2, 13.0 }, 0, false},
+	{ { 23.2, 23.2 }, 0, false},
+	{ { 23.2, 34.9 }, 0, false},
+	{ { 23.2, 46.7 }, 0, false},
+	{ { 23.2, 58.4 }, 0, false},
+	{ { 23.2, 70.2 }, 0, false},
+	{ { 34.9, 2.9 }, 0, false},
+	{ { 34.9, 23.2 }, 0, false},
+	{ { 46.7, 2.9 }, 0, false},
+	{ { 46.7, 23.2 }, 0, false},
+	{ { 46.7, 46.7 }, 0, false},
+	{ { 46.7, 58.4 }, 0, false},
+	{ { 58.4, 2.9 }, 0, false },
+	{ { 58.4, 23.2 }, 0, false },
+	{ { 58.4, 46.7 }, 0, false },
+	{ { 58.4, 70.2 }, 0, false },
+	{ { 70.2, 2.9 }, 0, false },
+	{ { 70.2, 23.2 }, 0, false },
+	{ { 70.2, 58.4 }, 0, false },
+	{ { 70.2, 82.1 }, 0, false },
+	{ { 82.1, 70.2 }, 0, false },
+	{ { 82.1, 93.9 }, 0, false },
+	{ { 82.1, 139.2 }, 0, false },
+	{ { 93.9, 82.1 }, 0, false },
+	{ { 93.9, 93.9 }, 0, false },
+	{ { 93.9, 117.5 }, 0, false },
+	{ { 93.9, 137.8 }, 0, false },
+	{ { 105.8, 117.5 }, 0, false },
+	{ { 105.8, 137.8 }, 0, false },
+	{ { 117.5, 93.9 }, 0, false },
+	{ { 117.5, 105.8 }, 0, false },
+	{ { 117.5, 117.5 }, 0, false },
+	{ { 117.5, 127.6 }, 0, false },
+	{ { 117.5, 137.8 }, 0, false },
+	{ { 127.6, 117.5 }, 0, false },
+	{ { 127.6, 127.6 }, 0, false },
+	{ { 127.6, 137.8 }, 0, false },
+	{ { 137.8, 82.1 }, 0, false },
+	{ { 137.8, 93.9 }, 0, false },
+	{ { 137.8, 105.8 }, 0, false },
+	{ { 137.8, 117.5 }, 0, false },
+	{ { 137.8, 127.6 }, 0, false },
+	{ { 137.8, 137.8 }, 0, false }
+}; 
+
+void field::initializeField() {
+	c.assign(&initialConfiguration[0], &initialConfiguration[54]);//assigns valeus to the vector of cones, from first parameter (0) to last one (53)
+}
+void field::FieldUpdate() {
+	if (!initialized) {
+		initializeField();
+	}
+
+}
+
+
+//constructor
+ field::field() : initialized(false) {
+	 c.assign(&initialConfiguration[0], &initialConfiguration[54]);//assigns valeus to the vector of cones, from first parameter (0) to last one (53)
+}
